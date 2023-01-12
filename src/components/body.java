fe09be79017da317282061bd1434bc6791fcf4a9
HP <callmesammy@yahoo.com>
2023-01-12 21:35:08 +0100

    files

--- src/components/body.java
+++ src/components/body.java
@@ -1,9 +1,8 @@
-
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JPanel.java to edit this template
+ */
 package components;
-
-import java.awt.Color;
-import net.miginfocom.swing.MigLayout;
-import swing.ScrollBar;
 
 /**
  *
@@ -16,9 +15,6 @@ public class body extends javax.swing.JPanel {
      */
     public body() {
         initComponents();
-        body.setLayout(new MigLayout("fillx"));
-        skm.setVerticalScrollBar(new ScrollBar());
-        skm.getVerticalScrollBar().setBackground(new Color(255,255,255));
     }
 
     /**
@@ -30,41 +26,19 @@ public class body extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        skm = new javax.swing.JScrollPane();
-        body = new javax.swing.JPanel();
-
-        skm.setBorder(null);
-
-        body.setBackground(new java.awt.Color(255, 255, 255));
-
-        javax.swing.GroupLayout bodyLayout = new javax.swing.GroupLayout(body);
-        body.setLayout(bodyLayout);
-        bodyLayout.setHorizontalGroup(
-            bodyLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 456, Short.MAX_VALUE)
-        );
-        bodyLayout.setVerticalGroup(
-            bodyLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 390, Short.MAX_VALUE)
-        );
-
-        skm.setViewportView(body);
-
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(skm)
+            .addGap(0, 400, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(skm, javax.swing.GroupLayout.DEFAULT_SIZE, 390, Short.MAX_VALUE)
+            .addGap(0, 300, Short.MAX_VALUE)
         );
     }// </editor-fold>//GEN-END:initComponents
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JPanel body;
-    private javax.swing.JScrollPane skm;
     // End of variables declaration//GEN-END:variables
 }
